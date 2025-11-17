f0a3ada7694f70f576ff54521d1ac7ecfcea603b
Toshiaki Maki <makingx@gmail.com>
2025-11-17 14:23:12 +0900

    Add counter log
    Signed-off-by: Toshiaki Maki <makingx@gmail.com>

--- src/main/java/am/ik/blog/counter/web/CounterController.java
+++ src/main/java/am/ik/blog/counter/web/CounterController.java
@@ -3,6 +3,8 @@ package am.ik.blog.counter.web;
 import am.ik.blog.counter.Counter;
 import am.ik.blog.counter.CounterMapper;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,8 @@ public class CounterController {
 
 	private final CounterMapper counterMapper;
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	public CounterController(CounterMapper counterMapper) {
 		this.counterMapper = counterMapper;
 	}
@@ -20,6 +24,10 @@ public class CounterController {
 	@PostMapping(path = "/counter")
 	public CounterResponse increment(@RequestBody CounterRequest request) {
 		Counter counter = this.counterMapper.increment(request.entryId());
+		logger.atInfo()
+			.addKeyValue("entryId", request.entryId())
+			.addKeyValue("counter", counter.counter())
+			.log("Counter incremented");
 		return new CounterResponse(counter.counter());
 	}
 
